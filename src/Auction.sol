9efc8b1071daa36d01d5f8b22726b54bb11994a0
AdamLeeeee <ly1130399693@gmail.com>
2024-10-23 16:26:05 +0800

    feat: waitingReturns

--- src/Auction.sol
+++ src/Auction.sol
@@ -6,7 +6,7 @@ contract Auction {
     address payable public owner;
     address public highestBidder;
     uint256 public highestBid;
-    mapping(address => uint256) public bids;
+    mapping(address => uint256) public waitingReturns;
     mapping(address => uint256) public coolTime;
     uint256 public coolTimeLimit;
     uint256 public endTime;
@@ -16,22 +16,29 @@ contract Auction {
     event AuctionEnded(address indexed highestBidder, uint256 highestBid);
 
     constructor(uint256 _startingPrice, uint256 _endTime, uint256 _coolTimeLimit) {
+        require(_startingPrice > 0, "Starting price must be greater than 0");
+        require(_endTime > block.timestamp, "End time must be in the future");
+        require(_coolTimeLimit > 0, "Cool time limit must be greater than 0");
+
         startingPrice = _startingPrice;
         endTime = _endTime;
         coolTimeLimit = _coolTimeLimit;
+        highestBid = startingPrice - 1;
         owner = payable(msg.sender);
     }
 
     function bid() public payable {
         require(block.timestamp < endTime, "Auction already ended.");
-        require(msg.value > 0, "Bid must be greater than 0.");
         require(coolTime[msg.sender] < block.timestamp, "You must wait for the cool time to pass.");
-        require(bids[msg.sender] + msg.value > highestBid, "There already is a higher bid.");
+        require(msg.value > highestBid, "There already is a higher bid.");
+
+        if (highestBid != 0) {
+            waitingReturns[highestBidder] += highestBid;
+            waitingReturns[msg.sender] = 0;
+        }
 
         highestBidder = msg.sender;
-        payable(highestBidder).transfer(msg.value);
-        bids[highestBidder] += msg.value;
-        highestBid = bids[highestBidder];
+        highestBid = msg.value;
         coolTime[highestBidder] = block.timestamp + coolTimeLimit;
 
         if (block.timestamp > endTime - coolTimeLimit) {
@@ -42,25 +49,28 @@ contract Auction {
     }
 
     function withdraw() public returns (bool) {
-        require(bids[msg.sender] > 0, "No funds to withdraw.");
-        require(
-            ended || msg.sender != highestBidder, "Only the highest bidder can not withdraw before the auction ends."
-        );
+        require(waitingReturns[msg.sender] > 0, "No funds to withdraw.");
 
-        payable(msg.sender).transfer(bids[msg.sender]);
-        bids[msg.sender] = 0;
-        coolTime[msg.sender] = 0;
+        uint256 amount = waitingReturns[msg.sender];
+        waitingReturns[msg.sender] = 0;
+
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Transfer failed.");
 
         return true;
     }
 
-    function end() public {
+    function end() public returns (bool){
+        require(msg.sender == owner, "Only the owner can end the auction.");
         require(block.timestamp >= endTime, "Auction not yet ended.");
         require(!ended, "end function has already been called.");
 
-        payable(owner).transfer(highestBid);
+        (bool success, ) = payable(owner).call{value: highestBid}("");
+        require(success, "Transfer failed.");
         ended = true;
 
         emit AuctionEnded(highestBidder, highestBid);
+
+        return true;
     }
 }
